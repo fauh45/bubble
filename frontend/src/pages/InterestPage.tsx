faf1807a38111ba0776740fcf9b0e8b046e0a408
Icad <irsyadmuhammad.020201@gmail.com>
2021-07-05 14:46:20 +0700

    Add timeline's interest page description card

---
+++ frontend/src/pages/InterestPage.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Box } from 'grommet';
+import Page from '../components/Page';
+
+
+class InterestPage extends React.Component{
+
+    render(){
+        return(
+            <Page>as
+                
+            </Page>
+        );
+    }
+}
+
+export default InterestPage;
